0f7ba286fbb2c3dde956ded072ac1cda037f54bd
hubLuo <hubluo@126.com>
2018-05-19 01:37:25 +0800

    实例3轮播图--引入RN自身组件ScrollView 1：实例1，2使用RN自身组件--TouchableOpacity，TouchableHighlight（补） 2：ScrollView（滚动视图）的组件会简单粗暴地把所有子元素一次性全部渲染出来。ListView会惰性渲染子元素，只在它们将要出现在屏幕中时开始渲染。FlatList是0.43版本开始新出的改进版的ListView，性能更优。

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
+
 import React, {Component} from 'react';
 
-const BagView = require('./components/BagView');
-const LoginView = require('./components/LoginView');
+const MyScrollView = require('./components/MyScrollView');
 
 export default class first extends Component {
     render() {
-     /*    return <BagView />*/
-        return <LoginView />
+        return <MyScrollView/>
     }
 }
